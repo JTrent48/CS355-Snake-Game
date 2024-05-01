d76897a8cd4c190858996b59f198d0a8a2fad3e7
JTrent48 <166766999+JTrent48@users.noreply.github.com>
2024-05-01 14:36:51 -0400

    Update Final (2).c
    With trophy and check trophy collission (not working lol)

--- Final (2).c
+++ Final (2).c
@@ -6,19 +6,18 @@
 #include <ncurses.h>
 
 
+
 #define DELAY 30000
 
-
+int random_direction;
+int directions[] = {KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT};
 //function protoypes
-int getRanNum();
 void makeBdr();
 void moveSnake();
-
-
-
-int getRanNum(){
-        return rand() % (max - min + 1) + min;
-}
+bool checkCollision(int x[], int y[], int length);
+void Trophy();
+int trophyX;
+int trophyY;
 
 
 
@@ -72,6 +71,70 @@ void makeBdr(){
       
 }
 
+void Trophy(){
+    int minX = 0; 
+    int maxX = COLS-1; 
+
+    int minY = 0;
+    int maxY = LINES - 1; 
+
+    trophyX = minX + rand() % (maxX - minX + 1); //Calculates random x value
+    trophyY = minY + rand() % (maxY - minY + 1); //Calculates random y value
+
+    //int num_positions = 10;
+
+    mvaddch(trophyY, trophyX, '*'); //Add trophy to random location
+
+    refresh();
+
+    //napms(3000);
+
+    //mvaddch(randY, randX, ' '); //Remove trophy from location
+
+    //refresh();
+
+     /*int trophyTimer = 0;
+        int trophyDuration = 5;
+        Trophy(num_positions);
+        if(trophyTimer >= trophyDuration){
+            mvaddch(randY, randX, ' ');
+        }
+        else{
+            trophyTimer ++;
+        }*/
+        
+    
+
+     
+}
+
+bool checkCollision(int x[], int y[], int length){
+    int width = COLS;
+    int height = LINES;
+
+    if(x[0] == 0 || x[0] >= COLS || y[0] == 0 || y[0] >= LINES){ //Checks for collision with walls
+        return true;
+    } 
+
+    for(int i = 1; i<length; i++){
+        if(x[0] == x[i] && y[0] == y[i]){ //Checks for collision with body
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool checkTrophyCollision(int snakeX[], int snakeY[]){
+    if(snakeX[0] == trophyX && snakeY[0] == trophyY){
+        Trophy();
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
 void moveSnake(){
     //int x = COLS/2;
     //int y = LINES/2;
@@ -79,40 +142,20 @@ void moveSnake(){
     int x[100] = {COLS/2, COLS/2-1, COLS/2 -2, COLS/2 -3, COLS/2 - 4}; //x location for snake
     int y[100] = {LINES/2, LINES/2, LINES/2, LINES/2, LINES/2}; //y location for snake
     int length = 5; //Initial length of snake
-    int inDir = getRandomNumber(1, 4);
-    int direction = KEY_RIGHT; //Initial direction of snake
+    int direction = random_direction; //Initial direction of snake
+    int num_positions = 10; //Number of positions trophy can spawn in
+
+    //PLace the initial trophy
+    Trophy();
 
     cbreak();
     noecho();
     curs_set(0);
     keypad(stdscr, TRUE);
-    nodelay(stdscr, TRUE); 
-     switch (inDir)
-    {
-    // Face right
-    case 1:
-        x[0] = 0;
-        y[0] = 1;
-        break;
-    // Face left
-    case 2:
-        x[0] = 0;
-        y[0] = -1;
-        break;
-    // Face up
-    case 3:
-        x[0] = -1;
-        y[0] = 0;
-        break;
-    // Face down
-    case 4:
-        x[0] = 1;
-        y[0] = 0;
-        break;
-    default:
-        break;
-    }
-        
+    nodelay(stdscr, TRUE);
+
+    
+
     while(1){
         int ch = getch();
          if (ch != ERR){
@@ -140,10 +183,16 @@ void moveSnake(){
         }
     }
 
+        checkTrophyCollision(x, y);
+
+        if(length > 1){
         for(int i = length -1; i>0; i--){
             x[i] = x[i-1];
             y[i] = y[i-1];
         }
+    }
+
+        
 
         switch(direction){ //Controls the direction of the snake based on the keys
             case KEY_UP:
@@ -160,28 +209,58 @@ void moveSnake(){
                 break;
         }
 
+        if(checkCollision(x, y, length)){
+            endwin();
+            return;
+        }
+
         clear();
         makeBdr();
         //mvaddch(y, x, 'o');
         //refresh();
-        for(int i = 0; i < length; i++){
+        mvaddch(y[0], x[0], 'o');
+        for(int i = 1; i<length; i++){
+            if(x[i] >=0 && x[i] < COLS && y[i] >=0 && y[i] < LINES){
             mvaddch(y[i], x[i], 'o');
         }
 
-        refresh();
-
-         usleep(DELAY);
         
     }
 
-    refresh();
+    
+         
+        
+        
+        
+        
+        refresh();
 
-    usleep(DELAY);
+         //timeout(DELAY);
+         usleep(100000);
+        
+    }
+
+    //refresh();
+
+    //usleep(DELAY);
 }
+
 int main(int argc, char *argv[]){
+
     initscr();
+    
+    srand(time(NULL));
+    
+    int numDirections = sizeof(directions) / sizeof(directions[0]);
+    random_direction = directions[rand() % numDirections];
+    int num_positions = 10; //Number of positions trophy can spawn in
+
     makeBdr();
     moveSnake();
+
+
+    
+    
     endwin();
     return 0;
 }
